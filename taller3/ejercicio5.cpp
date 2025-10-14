ae9658ae589b85cc30a9ca0021219b6d6cd5c821
Mariavalencia1 <maria.valencia03@uptc.edu.co>
2025-10-14 20:46:14 +0000

    ..

--- taller3/ejercicio5.cpp
+++ taller3/ejercicio5.cpp
@@ -1,2 +1,38 @@
 #include <iostream>
 using namespace std;
+
+
+bool esPalindromo(string palabra) {
+    int inicio = 0;
+    int fin = palabra.length() - 1;
+    while (inicio < fin) {
+        if (palabra[inicio] != palabra[fin]) {
+            return false;
+        }
+        inicio++;
+        fin--;
+    }
+    return true;
+}
+
+int main() {
+    string palabra1, palabra2;
+    
+    cout << "Ingrese la primera palabra:";
+    cin >> palabra1;
+    
+    cout << "Ingrese la segunda palabra:";
+    cin >> palabra2;
+    
+    if (esPalindromo(palabra1))
+        cout << palabra1 << " es un palindromo.";
+    else
+        cout << palabra1 << " no es un palindromo";
+    
+    if (esPalindromo(palabra2))
+        cout << palabra2 << " es un palindrono.";
+    else
+        cout << palabra2 << " no es un palindromo.";
+
+    return 0;
+}
