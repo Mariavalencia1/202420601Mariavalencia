2ec017f7ebbf04a21b3ebed102529e4270818cce
Mariavalencia1 <maria.valencia03@uptc.edu.co>
2025-10-14 19:49:46 +0000

    taller 3 1 y 2

---
+++ taller3/ejercicio2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string palabra;
+    cout << "Ingresa una pabalra";
+    cin >> palabra;
+    string resultado = "";
+    for (int i = 0; i < palabra.length(); i++) {
+        char letra = palabra[i];
+
+        if (i % 2 == 0) {
+            resultado += toupper(letra);
+        } 
+        else {
+            resultado += tolower(letra);
+        }
+    }
+    cout << "Resultado: " << resultado << endl;
+    return 0;
+}
